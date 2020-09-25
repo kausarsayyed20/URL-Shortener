16f2749c21906f795fbef94cc56981421dd0edc3
kausarsayyed20 <kausarsayyed.20@gmail.com>
2020-09-25 19:15:50 +0530

    Updated evn variable

--- DB/Connection.js
+++ DB/Connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-const URI="mongodb+srv://User2:root@cluster0.xtqpb.mongodb.net/FCCdb?retryWrites=true&w=majority";
+const URI="mongodb+srv://User2:"+process.env.PW+"@cluster0.xtqpb.mongodb.net/FCCdb?retryWrites=true&w=majority";
 
 const connectDB=()=>{
     mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true }); 
